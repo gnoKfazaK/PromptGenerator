1c98e8f8ab5cbee9099b378e60860a3714f8bcf2
gnoKfazaK <kazafkong2003@gmail.com>
2025-10-17 18:40:58 +0800

    Delete ollama directory

--- ollama/main.py
+++
@@ -1,8 +0,0 @@
-from promtGeneration import promptGenerator
-
-user_input = 'weather of hong kong today'
-
-p = promptGenerator()
-
-print(p.generate_prompt(user_prompt=user_input)[0])
-print(str(p.generate_prompt(user_prompt=user_input)[1]) + 's')
